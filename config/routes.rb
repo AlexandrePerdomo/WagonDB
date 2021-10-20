e48397f13cf29653cb5ba8d1288ead6077f51d8c
Alexandre Perdomo <aperdomo@free.fr>
2021-10-20 17:07:41 +0200

    cleaning front

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   root to: 'pages#home'
-  post 'xml' => 'pages#convert_from_xml'
-  post 'schema' => 'pages#convert_from_schema'
+  post 'convert_from_xml' => 'pages#convert_from_xml'
+  post 'convert_from_schema' => 'pages#convert_from_schema'
 end
